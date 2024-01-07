6082c0a4471f63cb25dbccb78852c7740671b503
Artur Rashkevich <arashkevich25@gmail.com>
2024-01-07 10:47:14 +0100

    add drawer

--- src/components/flp-drawer/index.ts
+++ src/components/flp-drawer/index.ts
@@ -1,6 +1,12 @@
 import { FlpDrawer as FlpDrawerRaw } from './flp-drawer.js';
 import * as React from 'react';
 import { EventName, createComponent } from '@lit/react';
+import { FlpAfterHide } from '../../events/flp-after-hide.js';
+import { FlpAfterShow } from '../../events/flp-after-show.js';
+import { FlpInitialFocus } from '../../events/flp-initial-focus.js';
+import { FlpRequestClose } from '../../events/flp-request-close.js';
+import { FlpShow } from '../../events/flp-show.js';
+import { FlpHide } from '../../events/flp-hide.js';
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -13,6 +19,12 @@ const FlpDrawer = createComponent({
   elementClass: FlpDrawerRaw,
   react: React,
   events: {
+    onFlpAfterHide: 'flp-after-hide' as EventName<FlpAfterHide>,
+    onFlpAfterShow: 'flp-after-show' as EventName<FlpAfterShow>,
+    onFlpInitialFocus: 'flp-initial-focus' as EventName<FlpInitialFocus>,
+    onFlpRequestClose: 'flp-request-close' as EventName<FlpRequestClose>,
+    onFlpShow: 'flp-show' as EventName<FlpShow>,
+    onFlpShowHide: 'flp-hide' as EventName<FlpHide>,
   },
   displayName: 'FlpDrawer'
 });
