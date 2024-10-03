ab1ba6211f81892e81c89e9e1b2472a86a73ead8
Artur Rashkevich <arashkevich25@gmail.com>
2024-10-03 17:22:02 +0200

    wip

--- src/containers/flp-login/flp-login.stories.ts
+++ src/containers/flp-login/flp-login.stories.ts
@@ -1,34 +1,34 @@
-// import { html } from 'lit';
-// import { Story } from '../../types/storybook';
-// import { Login } from './flp-login.types';
-// import "./../../components/flp-card";
-// import "./../../components/flp-button";
-// import "./../../components/flp-input";
-// import "./../../components/flp-logo";
-// import "./../../components/flp-divider";
-// import './flp-login';
-// import './../../themes/light.css';
-// import './../../themes/dark.css';
+import { html } from 'lit';
+import { Story } from '../../types/storybook';
+import { Login } from './flp-login.types';
+import "./../../components/flp-card";
+import "./../../components/flp-button";
+import "./../../components/flp-input";
+import "./../../components/flp-logo";
+import "./../../components/flp-divider";
+import './flp-login';
+import './../../themes/light.css';
+import './../../themes/dark.css';
 
-// export default {
-//   title: 'containers/Login',
-//   component: 'flp-login',
-// }
+export default {
+  title: 'containers/Login',
+  component: 'flp-login',
+}
 
-// const defaultProps: Login = {};
+const defaultProps: Login = {};
 
-// const Template: Story<Login> = (
-//   props: Login,
-//   {
-//     globals: { theme },
-//   }: {
-//     globals: { theme: string };
-//   }
-// ) => html`
-//   <div class="flp-theme-light">
-//     <flp-login tenant_key="some key" login_callback="some login callback" logout_callback="som logout callback"></flp-login>
-//   </div>
-// `;
+const Template: Story<Login> = (
+  props: Login,
+  {
+    globals: { theme },
+  }: {
+    globals: { theme: string };
+  }
+) => html`
+  <div class="flp-theme-light">
+    <flp-login tenant_key="some key" login_callback="some login callback" logout_callback="som logout callback"></flp-login>
+  </div>
+`;
 
-// export const Default = Template.bind({});
-// Default.args = defaultProps;
+export const Default = Template.bind({});
+Default.args = defaultProps;
