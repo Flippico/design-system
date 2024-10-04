40735657a2bae2abe7d149b2bba895287a7c8577
arashkevich25 <arashkevich25@gmail.com>
2024-10-04 07:27:42 +0000

    Deploying to gh-pages from @ Flippico/design-system@5eab34e80a131ad018033e98f5728afdc3bcb358 🚀

---
+++ syntaxhighlighter-NMPM6SWI-p0flhi6h.js
@@ -0,0 +1,2 @@
+import{S as c,c as l,s as n}from"./DocsRenderer-NNNQARDV-DH8qmgPP.js";import"./assets/iframe-CVaPrySK.js";import"./sb-preview/runtime.js";import"./index-D-l25SpK.js";import"./commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-CGpanSUo.js";import"./index-DrFu-skq.js";export{c as SyntaxHighlighter,l as createCopyToClipboardFunction,n as default};
+//# sourceMappingURL=syntaxhighlighter-NMPM6SWI-p0flhi6h.js.map
