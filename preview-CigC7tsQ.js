836c5a4f44bd21267744ebc30f29d70667b77cc4
arashkevich25 <arashkevich25@gmail.com>
2024-10-06 12:50:03 +0000

    Deploying to gh-pages from @ Flippico/design-system@d0301b87ae57dd6cab77b968c928443394acd49e 🚀

--- preview-CigC7tsQ.js
+++
@@ -1,8 +0,0 @@
-import{_ as t}from"./assets/iframe-CPRxzNhw.js";import"./sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-DpgUATWH.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-CigC7tsQ.js.map
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-DpgUATWH.js","./assets/iframe-CPRxzNhw.js","./index-D-l25SpK.js","./commonjsHelpers-Cpj98o6Y.js","./index-D-8MO0q_.js","./index-CGpanSUo.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
