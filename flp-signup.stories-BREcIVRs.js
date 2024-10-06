ea49f4824b766697bdba46a59b540f0fd3600cae
arashkevich25 <arashkevich25@gmail.com>
2024-10-06 17:59:30 +0000

    Deploying to gh-pages from @ Flippico/design-system@f8b2a6a2cd3bdaf4f7c29280e574c113f60658cb 🚀

--- flp-signup.stories-BREcIVRs.js
+++ flp-signup.stories-BREcIVRs.js
@@ -30,7 +30,7 @@ h2 {
   align-items: center;
   justify-content: center;
 }
-`;var w=Object.defineProperty,P=Object.getOwnPropertyDescriptor,l=(i,e,t,a)=>{for(var o=a>1?void 0:a?P(e,t):e,s=i.length-1,m;s>=0;s--)(m=i[s])&&(o=(a?m(e,t,o):m(o))||o);return a&&o&&w(e,t,o),o};let n=class extends y{constructor(){super(...arguments),this.tenantKey="",this.loginCallback="",this.logoutCallback="",this.name="",this.staging=!1,this.develop=!1,this.loginPending=!1}onSubmitHandle(i){i.preventDefault(),this.loginPending=!0;const e=new FormData(i.target);if(Array.from(e.values()).some(a=>a==="")){this.loginPending=!1;return}const t=new URLSearchParams;t.append("tenant_key",this.tenantKey),t.append("login_callback",this.loginCallback),t.append("logout_callback",this.logoutCallback),t.append("name",e.get("name")),t.append("email",e.get("email")),t.append("password",e.get("password")),fetch(`${c(this.staging,this.develop)}/api/${this.tenantKey}/signup`,{method:"POST",body:t,headers:{"Content-Type":"application/x-www-form-urlencoded"}}).then(a=>{a.status>=300&&a.status<400&&(window.location.href=a.headers.get("Location"))}).finally(()=>this.loginPending=!1)}render(){return b`<flp-card class="auth-container">
+`;var w=Object.defineProperty,P=Object.getOwnPropertyDescriptor,l=(i,a,t,e)=>{for(var o=e>1?void 0:e?P(a,t):a,s=i.length-1,m;s>=0;s--)(m=i[s])&&(o=(e?m(a,t,o):m(o))||o);return e&&o&&w(a,t,o),o};let n=class extends y{constructor(){super(...arguments),this.tenantKey="",this.loginCallback="",this.logoutCallback="",this.name="",this.staging=!1,this.develop=!1,this.loginPending=!1}onSubmitHandle(i){i.preventDefault(),this.loginPending=!0;const a=new FormData(i.target);if(Array.from(a.values()).some(e=>e==="")){this.loginPending=!1;return}const t=new URLSearchParams;t.append("tenant_key",this.tenantKey),t.append("login_callback",this.loginCallback),t.append("logout_callback",this.logoutCallback),t.append("name",a.get("name")),t.append("email",a.get("email")),t.append("password",a.get("password")),fetch(`${c(this.staging,this.develop)}/api/${this.tenantKey}/signup`,{method:"POST",body:t,headers:{"Content-Type":"application/x-www-form-urlencoded"}}).then(e=>{console.log(e),e.status>=300&&e.status<400&&(window.location.href=e.headers.get("Location"))}).finally(()=>this.loginPending=!1)}render(){return b`<flp-card class="auth-container">
       <form @submit=${this.onSubmitHandle}>
         <div class="logo-container text-align-center">
           <flp-logo></flp-logo>
@@ -55,7 +55,7 @@ h2 {
           <flp-button class="login--button" variant="text" href=${`${c(this.staging,this.develop)}/${this.tenantKey}/login`}>Login</flp-button>
         </div>
       </form>
-    </flp-card>`}};n.styles=[_];l([p({type:String,attribute:"tenant_key"})],n.prototype,"tenantKey",2);l([p({type:String,attribute:"login_callback"})],n.prototype,"loginCallback",2);l([p({type:String,attribute:"logout_callback"})],n.prototype,"logoutCallback",2);l([p({type:String,attribute:"name"})],n.prototype,"name",2);l([p({type:Boolean,attribute:"staging"})],n.prototype,"staging",2);l([p({type:Boolean,attribute:"develop"})],n.prototype,"develop",2);l([k()],n.prototype,"loginPending",2);n=l([v("flp-signup")],n);const X={title:"containers/Signup",component:"flp-signup"},x={},S=(i,{globals:{theme:e}})=>b`
+    </flp-card>`}};n.styles=[_];l([p({type:String,attribute:"tenant_key"})],n.prototype,"tenantKey",2);l([p({type:String,attribute:"login_callback"})],n.prototype,"loginCallback",2);l([p({type:String,attribute:"logout_callback"})],n.prototype,"logoutCallback",2);l([p({type:String,attribute:"name"})],n.prototype,"name",2);l([p({type:Boolean,attribute:"staging"})],n.prototype,"staging",2);l([p({type:Boolean,attribute:"develop"})],n.prototype,"develop",2);l([k()],n.prototype,"loginPending",2);n=l([v("flp-signup")],n);const X={title:"containers/Signup",component:"flp-signup"},x={},S=(i,{globals:{theme:a}})=>b`
   <div class="flp-theme-light">
     <flp-signup .develop=${!0} tenant_key="somekey" name="GDPR Plugin" login_callback="some login callback" logout_callback="som logout callback">
     </flp-signup>
@@ -74,4 +74,4 @@ h2 {
     </flp-signup>
   </div>
 \``,...(f=(d=r.parameters)==null?void 0:d.docs)==null?void 0:f.source}}};const Y=["Default"];export{r as Default,Y as __namedExportsOrder,X as default};
-//# sourceMappingURL=flp-signup.stories-CWIIik4r.js.map
+//# sourceMappingURL=flp-signup.stories-BREcIVRs.js.map
