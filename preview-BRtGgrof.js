b9a72482674d32bcb36e698984316cd2fecf2146
arashkevich25 <arashkevich25@gmail.com>
2024-10-06 16:03:46 +0000

    Deploying to gh-pages from @ Flippico/design-system@768811fd0568a45d7776ce4ad661f65b807ebed8 🚀

---
+++ preview-BRtGgrof.js
@@ -0,0 +1,8 @@
+import{_ as t}from"./assets/iframe-DgThALh6.js";import"./sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-CwhBo8My.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-BRtGgrof.js.map
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-CwhBo8My.js","./assets/iframe-DgThALh6.js","./index-D-l25SpK.js","./commonjsHelpers-Cpj98o6Y.js","./index-D-8MO0q_.js","./index-CGpanSUo.js","./index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
--- preview-dNiat0Wr.js
+++
@@ -1,8 +0,0 @@
-import{_ as t}from"./assets/iframe-CLwjsgXu.js";import"./sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-DsVvChyC.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-dNiat0Wr.js.map
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-DsVvChyC.js","./assets/iframe-CLwjsgXu.js","./index-D-l25SpK.js","./commonjsHelpers-Cpj98o6Y.js","./index-D-8MO0q_.js","./index-CGpanSUo.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
