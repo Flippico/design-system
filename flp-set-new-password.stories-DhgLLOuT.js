11308a6798087ff1a82c30351480126d49adde50
rafzei <webdler@gmail.com>
2025-07-03 21:32:53 +0000

    Deploying to gh-pages from @ Flippico/design-system@7c254f031f80e4c7a582ac71f8958fe5bd96144a 🚀

---
+++ flp-set-new-password.stories-DhgLLOuT.js
@@ -0,0 +1,75 @@
+import{i as u,x as i}from"./lit-element-ChAK2pu5.js";import{F as y,t as b}from"./custom-element-BSoDmtoY.js";import{n as p}from"./property-Bhw6601B.js";import{r as g}from"./state-dCtEkiMd.js";import{o as c}from"./if-defined-Bhm9Cegq.js";import{g as x}from"./get-api-url-CY5fWP8z.js";/* empty css             */const _=u`
+flp-button {
+  width: 100%;
+}
+.error {
+  color: var(--flp-color-warning-700);
+}
+h2 {
+  font-family: var(--flp-font-medium);
+}
+.auth-container {
+  max-width: 450px;
+  width: 100%;
+}
+.mb-small {
+  margin-bottom: 20px;
+}
+.mb-medium {
+  margin-bottom: 40px;
+}
+.text-align-center {
+  text-align: center;
+}
+.forgot-password-link--container {
+  text-align: right;
+  --flp-spacing-medium: 0px;
+}
+.login--button {
+  width: auto;
+}
+.login-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+`;var v=Object.defineProperty,P=Object.getOwnPropertyDescriptor,o=(r,e,n,t)=>{for(var a=t>1?void 0:t?P(e,n):e,d=r.length-1,m;d>=0;d--)(m=r[d])&&(a=(t?m(e,n,a):m(a))||a);return t&&a&&v(e,n,a),a};let s=class extends y{constructor(){super(...arguments),this.tenantKey="",this.token="",this.logo="",this.staging=!1,this.develop=!1,this.loginPending=!1,this.successState=!1}async onSubmitHandle(r){r.preventDefault(),this.loginPending=!0;const e=new FormData(r.target);if(Array.from(e.values()).some(t=>t==="")){this.errorText="Password is empty",this.loginPending=!1;return}const n=new URLSearchParams;if(!/^(?=.*\d.*\d)[A-Za-z\d!@#$%^&*()_+\-=[\]{};':",.<>?\/]{6,}$/.test(e.get("password"))){this.errorText="Minimum 8 znaków oraz minimum dwie liczby",this.loginPending=!1;return}if(e.get("new_password")!==e.get("password")){this.errorText="Hasła się różnią",this.loginPending=!1;return}n.append("new_password",e.get("new_password")),n.append("password",e.get("password")),n.append("token",e.get("token")),this.errorText=null,fetch(`${x(this.staging,this.develop)}/api/${this.tenantKey}/set-password/${e.get("token")}`,{method:"POST",body:n,headers:{"Content-Type":"application/x-www-form-urlencoded"}}).then(t=>{if(t.ok)return t.json();throw this.errorText="Something went wrong, try again",r.target.reset(),new Error("Something went wrong, try again")}).then(t=>{this.emit("flp-set-new-password-success"),window.parent.postMessage({type:"SET_NEW_PASSWORD_SUCCESS",payload:{}},"*"),window.location.href=t.message.redirect_url}).catch(console.error).finally(()=>this.loginPending=!1)}render(){return i`<flp-card class="auth-container">
+      <form @submit=${this.onSubmitHandle}>
+        <div class="logo-container text-align-center">
+          ${this.logo?i`<img .src=${this.logo} alt="logo" width="150" height="150" />`:i`<flp-logo></flp-logo>`}
+        </div>
+        <h2 class="text-align-center">Ustaw nowe hasło</h2>
+        <div class="mb-medium text-align-center">
+          <span class="text-align-center">Upewnij się, że nowe hasło zostało zapisane w bezpiecznym miejscu</span>
+        </div>
+        <flp-input class="mb-small" name="password" required type="password" label="Hasło" help-text="Minimum 8 znaków oraz minimum dwie liczby" password-toggle></flp-input>
+        <flp-input class="mb-small" name="new_password" required type="password" label="Powtórz hasło" password-toggle></flp-input>
+        <input name="token" value=${this.token} type="hidden">
+        ${this.successState?i`<maui-icon name="check-circle"></maui-icon>`:i`<flp-button
+          class="mb-small"
+          size="large"
+          variant="primary"
+          type="submit"
+          .loading=${c(this.loginPending)}
+          .disabled=${c(this.loginPending)}
+        >Ustaw nowe hasło</flp-button>`}
+        <div class="error">${this.errorText}</div>
+      </form>
+    </flp-card>`}};s.styles=[_];o([p({type:String,attribute:"tenant_key"})],s.prototype,"tenantKey",2);o([p({type:String,attribute:"token"})],s.prototype,"token",2);o([p({type:String,attribute:"logo"})],s.prototype,"logo",2);o([p({type:Boolean,attribute:"staging"})],s.prototype,"staging",2);o([p({type:Boolean,attribute:"develop"})],s.prototype,"develop",2);o([g()],s.prototype,"errorText",2);o([g()],s.prototype,"loginPending",2);o([g()],s.prototype,"successState",2);s=o([b("flp-set-new-password")],s);const U={title:"containers/SetNewPassword",component:"flp-set-new-password"},S={},k=(r,{globals:{theme:e}})=>i`
+  <div class="flp-theme-light">
+    <flp-set-new-password token="some_token" tenant_key="tenant_key"></flp-set-new-password>
+  </div>
+`,l=k.bind({});l.args=S;var w,h,f;l.parameters={...l.parameters,docs:{...(w=l.parameters)==null?void 0:w.docs,source:{originalSource:`(props: SetNewPassword, {
+  globals: {
+    theme
+  }
+}: {
+  globals: {
+    theme: string;
+  };
+}) => html\`
+  <div class="flp-theme-light">
+    <flp-set-new-password token="some_token" tenant_key="tenant_key"></flp-set-new-password>
+  </div>
+\``,...(f=(h=l.parameters)==null?void 0:h.docs)==null?void 0:f.source}}};const N=["Default"];export{l as Default,N as __namedExportsOrder,U as default};
+//# sourceMappingURL=flp-set-new-password.stories-DhgLLOuT.js.map
---
+++ flp-signup.stories-bxUDTcj3.js
@@ -0,0 +1,78 @@
+import{i as b,x as p}from"./lit-element-ChAK2pu5.js";import{F as y,t as w}from"./custom-element-BSoDmtoY.js";import{n as s}from"./property-Bhw6601B.js";import{r as h}from"./state-dCtEkiMd.js";import"./index-D2dJve2F.js";import{o as u}from"./if-defined-Bhm9Cegq.js";import{g as v}from"./get-api-url-CY5fWP8z.js";/* empty css             */import"./flp-card-D8iNYDgW.js";import"./component.styles-CK1ltfsO.js";import"./slot-BCTpRhJ5.js";import"./class-map-DFy0aMwt.js";import"./index-D2NdRWdU.js";import"./commonjsHelpers-Cpj98o6Y.js";import"./create-component-BisMVfBB.js";import"./flp-button-BZhtY4z0.js";import"./query-BApjzB0v.js";import"./form-DpMOf5Do.js";import"./watch-CEsCE2EF.js";import"./flp-input-BvhwxHaN.js";import"./form-control.styles-BBEJYdf5.js";import"./live-DswdT67c.js";import"./directive-helpers-DwFNumRl.js";import"./default-value-DaLIHyno.js";import"./flp-logo-CV8Fjdwo.js";import"./flp-divider-CsJRYlP4.js";const x=b`
+flp-button {
+  width: 100%;
+}
+.error {
+  color: var(--flp-color-warning-700);
+}
+h2 {
+  font-family: var(--flp-font-medium);
+}
+.auth-container {
+  max-width: 450px;
+  width: 100%;
+}
+.mb-small {
+  margin-bottom: 20px;
+}
+.mb-medium {
+  margin-bottom: 40px;
+}
+.text-align-center {
+  text-align: center;
+}
+.forgot-password-link--container {
+  text-align: right;
+  --flp-spacing-medium: 0px;
+}
+.login--button {
+  width: auto;
+}
+.login-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+`;var P=Object.defineProperty,_=Object.getOwnPropertyDescriptor,n=(r,e,l,i)=>{for(var t=i>1?void 0:i?_(e,l):e,m=r.length-1,g;m>=0;m--)(g=r[m])&&(t=(i?g(e,l,t):g(t))||t);return i&&t&&P(e,l,t),t};let o=class extends y{constructor(){super(...arguments),this.tenantKey="",this.logo="",this.loginUrl="",this.staging=!1,this.develop=!1,this.loginPending=!1}onSubmitHandle(r){r.preventDefault(),this.loginPending=!0;const e=new FormData(r.target);if(Array.from(e.values()).some(t=>t==="")){this.loginPending=!1;return}if(!/^(?=.*\d.*\d)[A-Za-z\d!@#$%^&*()_+\-=[\]{};':",.<>?\/]{6,}$/.test(e.get("password"))){this.errorText="Minimum 8 znaków oraz minimum dwie liczby",this.loginPending=!1;return}this.errorText=null;const l=new Headers;l.append("Content-Type","application/x-www-form-urlencoded");const i=new URLSearchParams;i.append("name",e.get("name")),i.append("email",e.get("email")),i.append("password",e.get("password")),fetch(`${v(this.staging,this.develop)}/api/${this.tenantKey}/signup`,{method:"POST",body:i,headers:l}).then(t=>{if(t.ok)return t.json();if(t.status===403)throw this.errorText="User already exist",r.target.reset(),new Error("User already exist")}).then(t=>{this.emit("flp-signup-success",t.message),window.parent.postMessage({type:"SIGNUP_SUCCESS",payload:t.message},"*"),window.location.href=t.message.redirect_url}).catch(console.error).finally(()=>this.loginPending=!1)}render(){return p`<flp-card class="auth-container">
+      <form @submit=${this.onSubmitHandle}>
+        <div class="logo-container text-align-center">
+          ${this.logo?p`<img .src=${this.logo} alt="logo" width="150" height="150" />`:p`<flp-logo></flp-logo>`}
+        </div>
+        <h2 class="text-align-center">Stwórz nowe konto</h2>
+        <flp-input class="mb-small"  type="text" required name="name" label="Imię"></flp-input>
+        <flp-input class="mb-small"  type="email" required name="email" label="Email"></flp-input>
+        <flp-input class="mb-medium"  name="password" help-text="Minimum 8 znaków oraz minimum dwie liczby" required type="password" label="Hasło" password-toggle></flp-input>
+        <flp-button
+          class="mb-small"
+          size="large"
+          variant="primary"
+          type="submit"
+          .loading=${u(this.loginPending)}
+          .disabled=${u(this.loginPending)}
+        >Zarejestruj się</flp-button>
+        <div class="login-container">
+          <span>Masz już konto?</span>
+          <flp-button class="login--button" variant="text" href=${this.loginUrl}>Zaloguj się</flp-button>
+        </div>
+        <div class="error">${this.errorText}</div>
+      </form>
+    </flp-card>`}};o.styles=[x];n([s({type:String,attribute:"tenant_key"})],o.prototype,"tenantKey",2);n([s({type:String,attribute:"logo"})],o.prototype,"logo",2);n([s({type:String,attribute:"login_url"})],o.prototype,"loginUrl",2);n([s({type:Boolean,attribute:"staging"})],o.prototype,"staging",2);n([s({type:Boolean,attribute:"develop"})],o.prototype,"develop",2);n([h()],o.prototype,"errorText",2);n([h()],o.prototype,"loginPending",2);o=n([w("flp-signup")],o);const X={title:"containers/Signup",component:"flp-signup"},S={},k=(r,{globals:{theme:e}})=>p`
+  <div class="flp-theme-light">
+    <flp-signup .develop=${!0} tenant_key="somekey" name="GDPR Plugin" login_callback="some login callback" logout_callback="som logout callback">
+    </flp-signup>
+  </div>
+`,a=k.bind({});a.args=S;var c,d,f;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`(props: Signup, {
+  globals: {
+    theme
+  }
+}: {
+  globals: {
+    theme: string;
+  };
+}) => html\`
+  <div class="flp-theme-light">
+    <flp-signup .develop=\${true} tenant_key="somekey" name="GDPR Plugin" login_callback="some login callback" logout_callback="som logout callback">
+    </flp-signup>
+  </div>
+\``,...(f=(d=a.parameters)==null?void 0:d.docs)==null?void 0:f.source}}};const Y=["Default"];export{a as Default,Y as __namedExportsOrder,X as default};
+//# sourceMappingURL=flp-signup.stories-bxUDTcj3.js.map
